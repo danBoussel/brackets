e78028b0e253d53b691704eaf5fde2f541dc2cb9
Daniil Boussel <daniil7142@gmail.com>
2021-03-12 02:46:24 -0500

    init: first try 13passing 7 not

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (str.includes(bracketsConfig[i].join(''))) {
+            str = str.replace(bracketsConfig[i].join(''), '')
+            if (str.length <= 0) return true
+        }
+    }
+    return false
 }
